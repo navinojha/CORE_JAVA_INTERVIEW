36c3e51bf15efa582922874829999472d894e629
Navin Ojha <navinojha1996@gmail.com>
2024-09-26 00:35:48 +0530

    Char Data type

---
+++ src/README.md
@@ -0,0 +1,12 @@
+# Variables:
+
+### variable is a container which holds the value
+
+```java
+int age = 20; # Here age is a variable
+```
+### In Java variables can be of two types:
+
+   1) Primitive Types
+   2) Non Primitive Types (Reference Type)
+
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+            1. char is of 2 bytes (It store the character representation of the ascii values)
+            2. Range is from 0 to 65435 (0 to 2^15)
+         */
+        char var = 'a';
+        System.out.println(var);
+
+        /*
+           1. bytes is of 1 bytes
+           2. Range is from -128 to +127
+           3>
+        */
+    }
+}
